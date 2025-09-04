b4f2a100536d6dd10ce6ff70a70826301083c1d8
Zasz <236136413+zaszrespawned@users.noreply.github.com>
2025-09-04 18:18:54 +0530

    Combined migrations into 1 file. systems table is just 1 column now. Fixed unit tests, manually tested, and update readme. Use uv and remove poetry.

--- db/test_recreate.sh
+++ db/test_recreate.sh
@@ -3,7 +3,5 @@
 DB=./tests/test_workspace/spyplane.db
 
 rm -rf "$DB"
-for file in ./db/migrations/*.sql; do
-  sqlite3 "$DB" < "$file"
-done
-sqlite3 "$DB" < ./db/data/spyplane_import.sql
+sqlite3 "$DB" < ./db/schema.sql
+sqlite3 "$DB" < ./db/data/import.sql
